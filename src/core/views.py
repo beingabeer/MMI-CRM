e8b68d1977cee3227ec2330ff584654dee87a9fb
abeer <abeer.sewak@gmail.com>
2020-04-03 13:55:20 -0500

    updated views

--- src/core/views.py
+++ src/core/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
-def home(request):
-    return render(request, 'dashboard.html')
+def dashboard(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_orders = orders.count()
+    total_customers = customers.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_orders': total_orders,
+        'total_customers': total_customers,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'products.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products.html', context)
 
 
-def customer(request):
-    return render(request, 'customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    total_orders = customer.order_set.all().count()
+    orders = customer.order_set.all()
+    context = {
+        'customer': customer,
+        'total_orders': total_orders,
+        'orders': orders
+    }
+    return render(request, 'customer.html', context)
