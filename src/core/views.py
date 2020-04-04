d03e41debe96dd18a59da3d3cec276c06e11eecc
abeer <abeer.sewak@gmail.com>
2020-04-04 15:04:08 -0500

    create, update and delete order logic

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 
 def index(request):
@@ -42,3 +43,37 @@ def customer(request, pk):
         'orders': orders
     }
     return render(request, 'customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/dashboard')
+
+    context = {'form': form}
+    return render(request, 'order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/dashboard')
+
+    context = {'form': form}
+    return render(request, 'order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/dashboard')
+    context = {'item': order}
+    return render(request, 'delete_form.html', context)
