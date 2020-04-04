021bba6a6fcd21d661cb03cf1699970f92e17ce0
abeer <abeer.sewak@gmail.com>
2020-04-04 16:42:12 -0500

    Updated customer form

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -45,15 +46,20 @@ def customer(request, pk):
     return render(request, 'customer.html', context)
 
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(
+        Customer, Order, fields=('product', 'status'), extra=5)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/dashboard')
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/dashboard/')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'order_form.html', context)
 
 
@@ -64,7 +70,7 @@ def updateOrder(request, pk):
         form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
-            return redirect('/dashboard')
+            return redirect('/dashboard/')
 
     context = {'form': form}
     return render(request, 'order_form.html', context)
@@ -74,6 +80,6 @@ def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
         order.delete()
-        return redirect('/dashboard')
+        return redirect('/dashboard/')
     context = {'item': order}
     return render(request, 'delete_form.html', context)
