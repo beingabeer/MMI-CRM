fdf7b0bd11ddd3e8f67e9792ae55e2e8aeebb64b
abeer <abeer.sewak@gmail.com>
2020-04-13 00:35:47 -0500

    added django filter to installed apps

--- src/accounts/settings/base.py
+++ src/accounts/settings/base.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -72,4 +73,3 @@ VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media')
-
